680a62d6fc51fb266be5b2d4c0603873f26e3d88
andres25ardila-cell <andres25ardila@gmail.com>
2025-11-27 08:34:09 -0500

    Add files via upload

---
+++ CARRO/carro_deportivo.py
@@ -0,0 +1,12 @@
+from vehiculo import Vehiculo
+
+class CarroDeportivo(Vehiculo):
+    def __init__(self, modelo, color, motor, numero_puertas, capacidad_pasajeros, tipo_combustible):
+        super().__init__(modelo, color, motor, numero_puertas, capacidad_pasajeros, tipo_combustible)
+        self.modo_sport = True
+
+    def aceleracion_frenado(self):  # Polimorfismo
+        return "Aceleración rápida y frenado deportivo."
+
+    def info_extra(self):
+        return f"Modo sport activo: {self.modo_sport}"
