d75a7e7e07dd4f33a59fcaab121c92de45a71c85
andres25ardila-cell <andres25ardila@gmail.com>
2025-12-13 22:20:03 -0500

    Delete libro.py

--- libro.py
+++
@@ -1,6 +0,0 @@
-from producto import Producto
-
-class libro(Producto):
-    def __init__(self, precio, titulo, autor, editorial, año, preferencias, genero):
-        super().__init__(precio, titulo, autor, editorial, año, preferencias)
-        self.genero = genero
