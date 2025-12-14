5925f0195d149b4630d2cf78735bbbbf934ea652
andres25ardila-cell <andres25ardila@gmail.com>
2025-12-13 22:19:53 -0500

    Delete indexador.py

--- indexador.py
+++
@@ -1,7 +0,0 @@
-class Indexador:
-    
-    def actualiza_almacen(self):
-        print("Actualizando almacen de productos")
-        
-    def envia_resultado_busqueda(self):
-        print("Enviando resultado de busqueda al usuario")
