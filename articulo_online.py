25d63695f4c41025a7fdc47766315291818c7078
andres25ardila-cell <andres25ardila@gmail.com>
2025-12-13 22:19:13 -0500

    Delete articulo_online.py

--- articulo_online.py
+++
@@ -1,9 +0,0 @@
-from producto import Producto
-
-class articulo_online(Producto):
-    def __init__(self, precio, titulo, autor, editorial, año, preferencias, tema):
-        super().__init__(precio, titulo, autor, editorial, año, preferencias)
-        self.tema = tema
-        
-    def publicar(self):
-        print(f"Se ha publicado el tema {self.tema}")
