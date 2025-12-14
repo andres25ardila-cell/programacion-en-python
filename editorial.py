fcea24125cb8b7feae0949b705ed2abdbdc597bd
andres25ardila-cell <andres25ardila@gmail.com>
2025-12-13 22:11:01 -0500

    Delete editorial.py

--- editorial.py
+++
@@ -1,9 +0,0 @@
-class Editorial:
-    
-    def __init__(self, nombre, direccion, telefono):
-        self.nombre = nombre
-        self.direccion = direccion
-        self.telefono = telefono
-        
-    def vender(self):
-        print(f"El producto ha sido vendido al editorial {self.nombre}")
