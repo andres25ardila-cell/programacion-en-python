822391deff43d5ec03c5076ed73e686f71c9bd96
andres25ardila-cell <andres25ardila@gmail.com>
2025-12-13 22:19:23 -0500

    Delete articulo_segunda_mano.py

--- articulo_segunda_mano.py
+++
@@ -1,8 +0,0 @@
-from producto import Producto
-
-class articulo_segunda_mano(Producto):
-    def __init__(self, precio, titulo, autor, editorial, año, preferencias, clasificacion, tema, vendedor):
-        super().__init__(precio, titulo, autor, editorial, año, preferencias)
-        self.clasificacion = clasificacion
-        self.tema = tema
-        self.vendedor = vendedor
